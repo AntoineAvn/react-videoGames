e0858bc825124a915dfda47dc7777dac5b2ea801
Antoine A <antoine_avenia@hotmail.fr>
2023-06-02 16:44:19 +0200

    import bootstrap + components + show footer + show Home

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
 import './index.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar.jsx";
 import NotFound from "./components/NotFound/NotFound.jsx";
+import Home from "./components/Home/Home.jsx";
+import Footer from './components/Footer/footer';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <NavBar />
 
     <Routes>
-      <Route path="/" element={<App />} />
-{/*       <Route path="/search" element={<SearchPage />} />
-      <Route path="/history" element={<HistoriquePage />} /> */}
+      <Route path="/" element={<Home />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
+
+    <Footer />
   </BrowserRouter>
 );
