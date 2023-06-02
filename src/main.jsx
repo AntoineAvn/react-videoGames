5bbd289ffc91450e31ddef3e67259837e5324248
Antoine A <antoine_avenia@hotmail.fr>
2023-06-02 10:42:59 +0200

    add Router

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar.jsx";
+import NotFound from "./components/NotFound/NotFound.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <NavBar />
+
+    <Routes>
+      <Route path="/" element={<App />} />
+{/*       <Route path="/search" element={<SearchPage />} />
+      <Route path="/history" element={<HistoriquePage />} /> */}
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
+);
