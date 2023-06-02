9c76afd5ff67b7e3c0059d9c538344d2a48532d5
Antoine A <antoine_avenia@hotmail.fr>
2023-06-02 16:43:19 +0200

    add footer

---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,12 @@
+import './footer.css'
+
+const Footer = () => {
+
+    return (
+      <div className="footer">
+            <p>Antoine Avenia © 2023</p>
+      </div>
+    )
+  }
+  
+  export default Footer
