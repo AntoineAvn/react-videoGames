4083791ac759c2fcff3d48b3ccbc3d954f694221
Antoine A <antoine_avenia@hotmail.fr>
2023-06-05 17:27:20 +0200

    add burger menu on mobile

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,22 +1,40 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.svg';
 import './NavBar.css';
 
-function NavBar(){
+function NavBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-      <div className="banniere">
-         <Link to="/" className='link-banniere'>
-            <div>
-                <img src={logo} className="img_logo" alt="logo" />
-                <h1 className="titre">GameShow</h1>
-            </div>
-          </Link>
-          <div>
-              <Link to="/" className='btn-nav'><button className="btn">Home</button></Link>
-              <Link to="/search" className='btn-nav'><button className="btn">Recherche</button></Link>
-              <Link to="/profil" className='btn-nav'><button className="btn">Profil</button></Link>
-          </div>
+    <div className="banniere">
+      <Link to="/" className="link-banniere">
+        <div>
+          <img src={logo} className="img_logo" alt="logo" />
+          <h1 className="titre">GameShow</h1>
+        </div>
+      </Link>
+      <div className={`menu ${isMenuOpen ? 'open' : ''}`}>
+        <button className="burger-menu" onClick={toggleMenu}>
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </button>
+        <Link to="/" className="btn-nav">
+          <button className="btn">Home</button>
+        </Link>
+        <Link to="/search" className="btn-nav">
+          <button className="btn">Recherche</button>
+        </Link>
+        <Link to="/profil" className="btn-nav">
+          <button className="btn">Profil</button>
+        </Link>
       </div>
+    </div>
   );
 }
 
